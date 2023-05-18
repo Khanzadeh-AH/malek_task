c7b57c8d3f2d535cf683c17f715acd3d07a8e097
AmirHossein <khanzadeh78ah@gmail.com>
2023-05-19 01:17:55 +0330

    :sparkles: Added web app

--- webapp/app.py
+++ webapp/app.py
@@ -1,15 +1,29 @@
 import os
 from pathlib import Path
 from flask import Flask, render_template
+import psycopg2
+
+
+def connect_to_db():
+    connection = psycopg2.connect(
+        host="db",
+        user=os.environ.get("POSTGRES_USER"),
+        password=os.environ.get("POSTGRES_PASSWORD"),
+        dbname=os.environ.get("POSTGRES_DB"),
+        port=5432
+    )
+    return connection
+
 
 current_dir = Path(__file__).resolve().parent
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def index():
-    return render_template(current_dir / "templates/index.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("WEBAPP_PORT")))
+    pg_conn = connect_to_db()
+    app.run(host="0.0.0.0", port=8080)
